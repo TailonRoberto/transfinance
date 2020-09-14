d842ee40c41a20714cd700967615f498ce20b689
rafael <djalmasantos839@gmail.com>
2020-09-14 14:41:59 -0300

    subindo desafio pronto 100%

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,23 +7,90 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome'; 
+
+}
+
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[];  
+  private balanceIncome: number;
+  private balanceOutcome: number;
+
+
 
   constructor() {
     this.transactions = [];
+    this.balanceIncome = 0;
+    this.balanceOutcome = 0;
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((accumulator: Balance, transation: Transaction ) =>
+     {   
+       //codigo aki
+       switch( transation.type )
+       {
+         case "income": 
+            accumulator.income += transation.value;
+            break;
+         case "outcome":
+            accumulator.outcome += transation.value;    
+            break;
+         default: 
+            break;  
+
+       }
+
+       return accumulator;
+
+
+     }, {  // formatação de retorno
+          income: 0, 
+          outcome: 0, 
+          total: 0  
+        }
+     )
+     
+      balance.total = balance.income - balance.outcome;
+      return balance;
+
+
+
+     // tinha feito assim antes e funfou que foi uma uva.
+    //  this.transactions.map(trans =>
+    //   {
+    //      if (trans.type === 'income')
+    //      {
+    //        this.balanceIncome =  this.balanceIncome + trans.value  
+    //      }else {
+    //        this.balanceOutcome = this.balanceOutcome + trans.value
+    //      }
+    //   });
+
+    //   return {
+    //     income: this.balanceIncome,
+    //     outcome: this.balanceOutcome,
+    //     total: (this.balanceIncome -  this.balanceOutcome)
+    //   }
   }
 
-  public create(): Transaction {
-    // TODO
+    public create({title,value,type}: CreateTransDTO ): Transaction {
+    const newTrans = new Transaction( {
+      title,
+      value,
+      type
+    });
+
+    this.transactions.push(newTrans);
+
+    return newTrans
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+ import TransactionsRepository from '../repositories/TransactionsRepository';
+ import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+ const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+
+    //return response.json(transactionsRepository.all()) ;
+
+    const transactions = transactionsRepository.all();   
+
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({transactions, balance}) ;
+    
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +26,20 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+    
+     const { title, value, type } = request.body;
+
+     const CreateTrans = new CreateTransactionService(transactionsRepository);     
+      
+     const NewTransation =  CreateTrans.execute( {title, value, type});
+      
+     return response.json(NewTransation);
+
+
+    
     // TODO
+
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
